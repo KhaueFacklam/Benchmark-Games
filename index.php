2b401493c6c5da26725bb4d3f14950830b5e8301
Khaue Tavares <99762206+KhaueFacklam@users.noreply.github.com>
2023-11-20 16:27:44 -0300

    -

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <html>
+
 <head>
     <title>Benchmark Games</title>
 </head>
+
 <body>
     <fieldset>
         <legend> Inicio </legend>
         <form name="entrada">
-            <a href="loginADM.php"><button type="button" name ="LoginADM"> Login de Administrador </button> </a>
+
+            <!-- Botão para login de administrador -->
+            <a href="loginADM.php"><button type="button" name="LoginADM"> Login de Administrador </button> </a>
+
+            <!-- Botão para login de usuário -->
             <a href="loginUser.php"><button type="button" name="LoginUser"> Login de Usuario</button></a>
+
+            <!-- Botão para cadastro de usuário -->
             <a href="cadastroUser.php"><button type="button" name="Cadastro"> Cadastro de Usuario</button></a>
         </form>
     </fieldset>
 </body>
+
 </html>
