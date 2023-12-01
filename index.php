723564369be8fda9e82d0c95fc6bdabcc10fe7a8
Khaue Tavares <99762206+KhaueFacklam@users.noreply.github.com>
2023-11-30 23:19:12 -0300

    Update

--- index.php
+++ index.php
@@ -6,15 +6,15 @@
 
 <body>
     <fieldset>
-        <legend> Iníciooo </legend>
+        <legend> Inicio </legend>
         <form name="entrada">
-            <!-- Botão para login de administrador  -->
+            <!-- Botão para login de administrador   -->
             <a href="loginADM.php"><button type="button" name="LoginADM"> Login de Administrador </button> </a>
 
-            <!-- Botão para login de usuário -->
+            <!-- Botão para login de usuário  -->
             <a href="loginUser.php"><button type="button" name="LoginUser"> Login de Usuario</button></a>
 
-            <!-- Botão para cadastro de usuário -->
+            <!-- Botão para cadastro de usuário  -->
             <a href="cadastroUser.php"><button type="button" name="Cadastro"> Cadastro de Usuario</button></a>
         </form>
     </fieldset>
